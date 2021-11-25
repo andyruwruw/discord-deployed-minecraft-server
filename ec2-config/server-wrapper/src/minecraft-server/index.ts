e5c4dc4fd86de930452ffca1aa7bc63efdfe4fa5
Andrew Young <46572910+andyruwruw@users.noreply.github.com>
2021-11-24 17:10:44 -0800

    Fixing cyrus stuff

--- ec2-config/server-wrapper/src/minecraft-server/index.ts
+++ ec2-config/server-wrapper/src/minecraft-server/index.ts
@@ -10,6 +10,7 @@ import {
   SERVER_JAR_DIR,
   SERVER_JAR_NAME,
 } from '../config/environment';
+import { MINECRAFT_ARGS } from 'src/config';
 
 /**
  * Generates a ScriptServer instance.
@@ -21,24 +22,7 @@ export const generateMinecraftServer = () => {
     javaServer: {
       path: SERVER_JAR_DIR,
       jar: SERVER_JAR_NAME,
-      args: ['-Xmx22000M', '-Xms22000M','-XX:+UseG1GC',
-      '-XX:+ParallelRefProcEnabled',
-      '-XX:MaxGCPauseMillis=200',
-      '-XX:+UnlockExperimentalVMOptions',
-      '-XX:+DisableExplicitGC',
-      '-XX:+AlwaysPreTouch',
-      '-XX:G1NewSizePercent=40',
-      '-XX:G1MaxNewSizePercent=50',
-      '-XX:G1HeapRegionSize=16M',
-      '-XX:G1ReservePercent=15',
-      '-XX:G1HeapWastePercent=5',
-      '-XX:G1MixedGCCountTarget=4',
-      '-XX:InitiatingHeapOccupancyPercent=20',
-      '-XX:G1MixedGCLiveThresholdPercent=90',
-      '-XX:G1RSetUpdatingPauseTimePercent=5',
-      '-XX:SurvivorRatio=32',
-      '-XX:+PerfDisableSharedMem',
-      '-XX:MaxTenuringThreshold=1',],
+      args: MINECRAFT_ARGS,
     },
     rconConnection: {
       port: parseInt(RCON_PORT as string, 10),
