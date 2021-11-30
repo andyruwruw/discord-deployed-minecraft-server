78688b5d50045cf36279b8aa81997fd91997b210
Andrew Young <46572910+andyruwruw@users.noreply.github.com>
2021-11-29 16:56:13 -0800

    '

--- discord-bot/src/copper-bot/responses/response.ts
+++ discord-bot/src/copper-bot/responses/response.ts
@@ -1,3 +1,16 @@
 export class DiscordResponse {
   
+  build() {
+    return {
+      content: null,
+      tts: false,
+      embeds: [],
+      allowedMentions: {},
+      messageReference: null,
+      components: [],
+      files: [],
+      payloadJson: '',
+      attatchments: [],
+    };
+  }
 }
